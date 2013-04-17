94b4e6da1540be60cd30125ced252841e95e2e76
actraiser <actraiser@dustlayer.com>
2013-04-17 10:56:55 +0200

    optimized  sub_colorwash.asm

--- code/sub_colorwash.asm
+++ code/sub_colorwash.asm
@@ -2,26 +2,24 @@
 ; color washer routine
 ;============================================================
 
-colwash    lda color+$00      ; load first color
-           sta color+$28      ; store in column 40 ($28 = #40) - this resets the table for the next color cycle
-           ldx #$00           ; init counter
-           sta $d9df,x
-cycle1     lda color+$01,x    ; CYCLE iterates once(!) by moving color by 1 in table
-           sta color+$00,x 
-           lda color,x        ; load current color
-           sta $d990,x        ; put into color ram for one of the 40 positions
-           inx                ; inc iterator
+colwash    lda color+$00      ; load the current first color from table
+           sta color+$28      ; store in in last position of table to reset the cycle
+           ldx #$00           ; init X with zero
+           sta $d9df,x        ; put the current color code into Color RAM position + X
+cycle1     lda color+1,x      ; Start cycle by fetching next color in the table...
+           sta color,x        ; ...and store it in the current active position.
+           sta $d990,x        ; put into Color Ram
+           inx                ; increment X-Register
            cpx #$28           ; have we done 40 iterations yet?
            bne cycle1         ; if no, continue
 
-colwash2   lda color2+$28     ; load last color
-           sta color2+$00     ; store in column 00 this resets the table for the next color cycle
+colwash2   lda color2+$28     ; load current last color from second table
+           sta color2+$00     ; store in in first position of table to reset the cycle
            ldx #$28
-cycle2     lda color2-$01,x   ; CYCLE iterates once(!) by moving color by 1 in table in reverse order
-           sta color2+$00,x 
-           lda color2,x       ; load current color
-           sta $d9df,x        ; put into color ram for one of the 40 positions
+cycle2     lda color2-1,x     ; Start cycle by fetching previous color in the table...
+           sta color2,x       ; ...and store it in the current active position.
+           sta $d9df,x        ; put into Color Ram
            dex                ; decrease iterator
            bne cycle2         ; if x not zero yet, continue
 
-           rts                ; return 
+           rts                ; return from subroutine
