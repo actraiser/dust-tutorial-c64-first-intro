0ead84251434e39a554b9088b902472be0ea75f7
Rocco <rocco@dileo.info>
2015-11-28 20:52:09 +0100

    colorwash routine was refactored to address a bug which read/wrote outside the color tables

--- code/sub_colorwash.asm
+++ code/sub_colorwash.asm
@@ -1,25 +1,23 @@
-;============================================================
-; color washer routine
-;============================================================
-
-colwash    lda color+$00      ; load the current first color from table
-           sta color+$28      ; store in in last position of table to reset the cycle
-           ldx #$00           ; init X with zero
-
-cycle1     lda color+1,x      ; Start cycle by fetching next color in the table...
-           sta color,x        ; ...and store it in the current active position.
-           sta $d990,x        ; put into Color Ram
-           inx                ; increment X-Register
-           cpx #$28           ; have we done 40 iterations yet?
-           bne cycle1         ; if no, continue
-
-colwash2   lda color2+$28     ; load current last color from second table
-           sta color2+$00     ; store in in first position of table to reset the cycle
-           ldx #$28
-cycle2     lda color2-1,x     ; Start cycle by fetching previous color in the table...
-           sta color2,x       ; ...and store it in the current active position.
-           sta $d9df,x        ; put into Color Ram
-           dex                ; decrease iterator
-           bne cycle2         ; if x not zero yet, continue
-
-           rts                ; return from subroutine
+colwash   ldx #$27        ; load x-register with #$27 to work through 0-39 iterations
+          lda color+$27   ; init accumulator with the last color from first color table
+cycle1    ldy color-1,x   ; use y-register to remember color at current position in iteration
+          sta color-1,x   ; write the stored color from accumulator to that location
+          sta $d990,x     ; put it into Color Ram
+          tya             ; transfer our remembered color back to accumulator
+          dex             ; decrement x-Register to go to next iteration
+          bne cycle1      ; repeat if there are iterations left
+          sta color+$27   ; otherwise store te final color into accumulator
+          sta $d990       ; ... and into Color Ram
+                          ; do the reverse for the second line of text in the intro
+colwash2  ldx #$00        ; load x-register with #$00 to track iterations
+          lda color2+$27  ; load the last color from the second table
+cycle2    ldy color2,x    ; remember currently pointed at color of second table in y-register
+          sta color2,x    ; store that color into accumulator
+          sta $d9e0,x     ; ... and write it to Color Ram
+          tya             ; transfer our remembered color back to accumulator 
+          inx             ; increment x-register to go to next iteraton
+          cpx #$26        ; have we gone through 39 iterations yet?
+          bne cycle2      ; if no, repeat
+          sta color2+$27  ; if yes, store the final color into accumulator
+          sta $d9e0+$27   ; and write it into Color Ram
+          rts             ; return from subroutine
