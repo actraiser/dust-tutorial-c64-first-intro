d7c5de7bc40804ed950c117348c8c455627e70a5
Rocco <rocco@dileo.info>
2015-11-28 21:56:34 +0100

    colorwash routine code comments updates

--- code/sub_colorwash.asm
+++ code/sub_colorwash.asm
@@ -1,5 +1,6 @@
 colwash   ldx #$27        ; load x-register with #$27 to work through 0-39 iterations
           lda color+$27   ; init accumulator with the last color from first color table
+
 cycle1    ldy color-1,x   ; use y-register to remember color at next position in iteration
           sta color-1,x   ; write the stored color from accumulator to that location
           sta $d990,x     ; put it into Color Ram
@@ -11,6 +12,7 @@ cycle1    ldy color-1,x   ; use y-register to remember color at next position in
                           ; do the reverse for the second line of text in the intro
 colwash2  ldx #$00        ; load x-register with #$00 to track iterations
           lda color2+$27  ; load the last color from the second table
+
 cycle2    ldy color2,x    ; remember previously used color of second table in y-register
           sta color2,x    ; store that color into accumulator
           sta $d9e0,x     ; ... and write it to Color Ram
@@ -20,4 +22,5 @@ cycle2    ldy color2,x    ; remember previously used color of second table in y-
           bne cycle2      ; if no, repeat
           sta color2+$27  ; if yes, store the final color into accumulator
           sta $d9e0+$27   ; and write it into Color Ram
+ 
           rts             ; return from subroutine
